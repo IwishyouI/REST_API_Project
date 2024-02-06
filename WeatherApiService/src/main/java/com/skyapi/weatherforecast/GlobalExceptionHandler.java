c5274d82ede441fe8752ccfc93610cdc41536d1b
IwishyouI <assameasyou11@gmail.com>
2024-02-06 10:51:20 +0900

    GlobalExceptioinHandler 작성 완료

--- WeatherApiService/src/main/java/com/skyapi/weatherforecast/GlobalExceptionHandler.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.skyapi.weatherforecast;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,19 +13,24 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @ControllerAdvice
-public class GlobalExceptionController {
+public class GlobalExceptionHandler {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
-    public ErrorDTO handleGenericException(HttpServletRequest request, Exception exception) {
+    public ErrorDTO handleGenericException(HttpServletRequest request, Exception ex) {
         ErrorDTO error = new ErrorDTO();
         error.setTimestamp(new Date());
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         error.setError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
         error.setPath(request.getServletPath());
 
+
+        LOGGER.error(ex.getMessage(),ex);
         return error;
     }
 }
