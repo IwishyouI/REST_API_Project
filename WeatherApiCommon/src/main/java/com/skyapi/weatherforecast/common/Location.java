a78320a058bdb13fe32a9845ecdc9a257cb22dd5
IwishyouI <assameasyou11@gmail.com>
2024-09-15 11:31:38 +0900

    [2024.09.15] Location entity added

--- WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -2,18 +2,40 @@ package com.skyapi.weatherforecast.common;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sun.istack.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "locations")
 public class Location {
+    @Column(length = 12, nullable = false, unique = true)
+    @Id
+    private String code;
+
+    @Column(length = 128, nullable = false)
+    private String cityName;
+
+    @Column(length = 128)
+    private String regionName;
+
+    @Column(length = 64, nullable = false)
+    private String countryName;
+
+    @Column(length = 2, nullable = false)
+    private String countryCode;
+
+    private boolean enabled;
+
+    @JsonIgnore
+    private boolean trashed;
+
 
 }
