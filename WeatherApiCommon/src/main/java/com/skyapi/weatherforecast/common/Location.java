9571df9ad433bf3ac9ede0bcb15969c104c6bac4
IwishyouI <assameasyou11@gmail.com>
2025-01-01 11:26:45 +0900

    create post mapping

--- WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -18,6 +18,7 @@ public class Location {
 
 
     @Column(length = 12, nullable = false, unique = true)
+    @Id
     private String code;
 
     @Column(length = 128, nullable = false)
@@ -35,6 +36,62 @@ public class Location {
     private boolean enabled;
     private boolean trashed;
 
+    public Location() {
+    }
 
+    public String getCode() {
+        return code;
+    }
 
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isTrashed() {
+        return trashed;
+    }
+
+    public void setTrashed(boolean trashed) {
+        this.trashed = trashed;
+    }
 }
---
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationApiController.java
@@ -0,0 +1,30 @@
+package com.skyapi.weatherforecast.location;
+
+
+import com.skyapi.weatherforecast.common.Location;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/locations")
+public class LocationApiController {
+
+    private LocationService service;
+
+    public LocationApiController(LocationService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Location> addLocation(@RequestBody Location location) {
+        Location addedLocation = service.add(location);
+        URI uri = URI.create("/v1/locations" + location.getCode());
+
+        return ResponseEntity.created(uri).body(addedLocation);
+    }
+}
